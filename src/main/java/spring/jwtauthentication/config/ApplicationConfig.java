67788db0ca0b685b13376c11162986760c6f9644
syk25 <syk2245@gmail.com>
2024-02-12 17:47:04 +0900

    feat:[feat - 005] 애플리케이션 환경설정 구현
    - 이메일을 토대로 사용자 정보 객체 반환

---
+++ src/main/java/spring/jwtauthentication/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package spring.jwtauthentication.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import spring.jwtauthentication.user.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not Found"));
+    }
+}
