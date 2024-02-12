225e7b45b0bb6a309ac89ff5da9ccbfa22a6c20b
syk25 <syk2245@gmail.com>
2024-02-12 18:04:28 +0900

    feat:[feat - 007] Filter Binding 구현
    필터를 만든 후에는 필터체인에 추가해야함.
    필터가 할 행동들을 정의하고 구현함.

---
+++ src/main/java/spring/jwtauthentication/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package spring.jwtauthentication.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("") // white list - 인증 불요
+                .permitAll()
+                .anyRequest() // white list 제외 인증 필요
+                .authenticated()
+                .and()
+                .sessionManagement()// 세션관리 - jwt로 인증하기 때문에 무상태성으로 전환
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
