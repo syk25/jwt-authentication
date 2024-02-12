8ccc3a997c7e2ca1cf17b5bb492ca25b7d92443c
syk25 <syk2245@gmail.com>
2024-02-12 18:19:51 +0900

    feat:[feat - 009] AuthenticationService 의 register 메서드 구현

--- src/main/java/spring/jwtauthentication/auth/AuthenticationService.java
+++ src/main/java/spring/jwtauthentication/auth/AuthenticationService.java
@@ -1,15 +1,33 @@
 package spring.jwtauthentication.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import spring.jwtauthentication.config.JwtService;
+import spring.jwtauthentication.user.Role;
+import spring.jwtauthentication.user.User;
+import spring.jwtauthentication.user.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        User user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken).build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
--- src/main/java/spring/jwtauthentication/user/Role.java
+++ src/main/java/spring/jwtauthentication/user/Role.java
@@ -1,4 +1,12 @@
 package spring.jwtauthentication.user;
 
+import lombok.RequiredArgsConstructor;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
 public enum Role {
+
+    USER;
+
 }
